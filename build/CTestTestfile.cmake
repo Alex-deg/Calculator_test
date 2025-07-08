82704dcc0339e089027ca816858cea2fbc827a55
Alex Degtyarev <degtyarevaleksandr2005@gmail.com>
2025-07-08 18:23:59 +0300

    Some Unit Tests

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/Протей/ДЗ №5 Tests/Calculator_test
+# Build directory: /home/alex/Протей/ДЗ №5 Tests/Calculator_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
